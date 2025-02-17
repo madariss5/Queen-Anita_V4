d7b12d96c0b48d2a0f024366ead007ca9678d6dd
madariss5 <menyhartmartin4@gmail.com>
2025-02-17 18:26:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); 
 
 global.owner = process.env.OWNER_NUMBER.split(','); 
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
+global.anitav4 = process.env.BOT_NAME || "Martin-𝐕𝟒";
 global.ownername = process.env.OWNER_NAME || "Martin V4";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
